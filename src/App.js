c861ab7ab92a74af3693d1f68ae7c7095d7695c1
Azami <99400189+ArseniMokhau@users.noreply.github.com>
2024-09-15 15:04:37 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,200 @@
-import logo from './logo.svg';
+import { Classrooms, Faculties, EmbedsByFaculty } from './data/Classrooms'
+import { QRCodeCanvas } from 'qrcode.react';
 import './App.css';
+import { useState } from 'react';
+
+function GetClassroomByName(name) {
+  return Classrooms.find(classroom => classroom.name === (name))
+}
+
+function GetFacultyByName(name) {
+  return Faculties.find(faculty => faculty.name === (name))
+}
 
 function App() {
+
+  const [startLocation, setStartLocation] = useState('')
+  const [endLocation, setEndLocation] = useState('')
+
+  const [startLocationDescription, setStartLocationDescription] = useState('')
+  const [endLocationDescription, setEndLocationDescription] = useState('')
+
+  const [startLocationImage, setStartLocationImage] = useState('')
+  const [endLocationImage, setEndLocationImage] = useState('')
+
+  const [startFilter, setStartFilter] = useState('')
+  const [endFilter, setEndFilter] = useState('')
+
+  const [embedURL, setEmbedURL] = useState('')
+  const [qrURL, setQRURL] = useState('')
+  
+  const [showStartList, setShowStartList] = useState(false);
+  const [showEndList, setShowEndList] = useState(false);
+
+
+  const getGoogleMapsEmbedUrl = (startLocation, endLocation) => {
+    const eURL = EmbedsByFaculty.find(url => url.startFaculty === startLocation.faculty && url.endFaculty === endLocation.faculty).embedURL
+    return eURL
+  }
+
+  const getGoogleMapsQRURL = (startLocation, endLocation) => {
+    return `https://www.google.com/maps/dir/?api=1&origin=${encodeURIComponent(startLocation)}&destination=${encodeURIComponent(endLocation)}&travelmode=walking`
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const start = GetClassroomByName(startLocation)
+    const end = GetClassroomByName(endLocation)
+
+    const startFaculty = GetFacultyByName(start.faculty)
+    const endFaculty = GetFacultyByName(end.faculty)
+    if (start && end && start !== end) {
+      const embedUrl = getGoogleMapsEmbedUrl(start, end)
+      setEmbedURL(embedUrl)
+      const qrUrl = getGoogleMapsQRURL(startFaculty.address, endFaculty.address)
+      setQRURL(qrUrl)
+    }
+  }
+
+  const filteredStartClassrooms = Classrooms.filter(classroom => classroom.name.toLowerCase().includes(startFilter.toLowerCase()))
+  const filteredEndClassrooms = Classrooms.filter(classroom => classroom.name.toLowerCase().includes(endFilter.toLowerCase()))
+
+  const handleStartLocationSelect = (name) => {
+    const location = GetClassroomByName(name);
+
+    setStartLocation(name)
+    setStartFilter(name)
+    setShowStartList(false)
+
+    if (location) {
+      const description = location.description
+      setStartLocationDescription(description)
+      
+      const image = location.image
+      setStartLocationImage(image)
+    }
+  }
+
+  const handleEndLocationSelect = (name) => {
+    const location = GetClassroomByName(name);
+
+    setEndLocation(name)
+    setEndFilter(name)
+    setShowEndList(false)
+
+    if (location) {
+      const description = location.description
+      setEndLocationDescription(description)
+      
+      const image = location.image
+      setEndLocationImage(image)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Left Section */}
+      <div className="left-section">
+        <h1>Select Start and End Locations</h1>
+        <form onSubmit={handleSubmit}>
+        <div className="form-group">
+            <label>Start Location:</label>
+            <input
+              type='text'
+              value={startFilter}
+              onChange={(e) => setStartFilter(e.target.value)}
+              placeholder='Type to filter'
+              onFocus={() => setShowStartList(true)}
+              onBlur={() => setTimeout(() => setShowStartList(false), 10)}>
+            </input>
+            {showStartList && startFilter && (
+              <ul className="autocomplete-list">
+                {filteredStartClassrooms.map((classroom) => (
+                  <li
+                    key={classroom.name}
+                    onMouseDown={() => handleStartLocationSelect(classroom.name)}
+                    className="autocomplete-item"
+                  >
+                    {classroom.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <div className="form-group">
+            <label>End Location:</label>
+            <input
+              type='text'
+              value={endFilter}
+              onChange={(e) => setEndFilter(e.target.value)}
+              placeholder='Type to filter'
+              onFocus={() => setShowEndList(true)}
+              onBlur={() => setTimeout(() => setShowEndList(false), 10)}>
+            </input>
+            {showEndList && endFilter && (
+              <ul className="autocomplete-list">
+                {filteredEndClassrooms.map((classroom) => (
+                  <li
+                    key={classroom.name}
+                    onMouseDown={() => handleEndLocationSelect(classroom.name)}
+                    className="autocomplete-item"
+                  >
+                    {classroom.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          <button type='submit'>Get Directions</button>
+        </form>
+
+        {embedURL && (
+          <div>
+            <h2>Directions</h2>
+            <iframe
+              title="Google Maps Directions"
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              src={embedURL}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+        )}
+
+        {qrURL && (
+          <div className='qr-code-container'>
+            <h3>Or Scan The QR Code to View on Mobile</h3>
+            <QRCodeCanvas value={qrURL} size={128}/>
+            <p>
+              <a href={qrURL} target="_blank" rel="noopener noreferrer">
+                Click here to open in Google Maps
+              </a>
+            </p>
+          </div>
+        )}
+      </div>
+
+      {/* Right Section */}
+      <div className="right-section">
+        {startLocation && startLocationDescription && (
+          <div className="building-description">
+          {startLocationImage && <img src={startLocationImage} alt={`${startLocation}`} className="building-image" />}
+          <h3>{startLocation}</h3>
+          <p>{startLocationDescription}</p>
+        </div>
+        )}
+        {endLocation && endLocationDescription && (
+        <div className="building-description">
+          {endLocationImage && <img src={endLocationImage} alt={`${endLocation}`} className="building-image" />}
+          <h3>{endLocation}</h3>
+          <p>{endLocationDescription}</p>
+        </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/data/Classrooms.js
@@ -0,0 +1,14 @@
+export const Classrooms = Object.freeze([
+    {name: '401 MS', faculty: 'MS', description: '401 MS description', image: '/images/wydzial-rms-slide.jpg'},
+    {name: '111 LB', faculty: 'LB', description: '111 LB description', image: '/images/wydzial_budownictwa_v3-1.jpg'},
+]);
+
+export const Faculties = Object.freeze([
+    {name: 'MS', address: 'Kaszubska 23, 44-100 Gliwice', description: 'MS description', image: '/images/wydzial-rms-slide.jpg'},
+    {name: 'LB', address: 'Bolesława Krzywoustego 7, 44-100 Gliwice', description: 'LB description', image: '/images/wydzial_budownictwa_v3-1.jpg'},
+]);
+
+export const EmbedsByFaculty = Object.freeze([
+    {startFaculty: 'MS', endFaculty: 'LB', embedURL: 'https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1274.523965657216!2d18.677949789074155!3d50.291033142890534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x47113101aa29eacb%3A0xb264c95270614eda!2zUG9saXRlY2huaWthIMWabMSFc2thLCBXeWR6aWHFgiBNYXRlbWF0eWtp4oCm!3m2!1d50.291104499999996!2d18.6800434!4m5!1s0x471131d58fee95a1%3A0x8610e12d1d9dc0ac!2sLaboratorium%20Budownictwa%E2%80%A6!3m2!1d50.2905893!2d18.67881!5e0!3m2!1sen!2spl!4v1725820044748!5m2!1sen!2spl'},
+    {startFaculty: 'LB', endFaculty: 'MS', embedURL: 'https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d1274.5529757238492!2d18.67926145320014!3d50.2899500738315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!1s0x471131d58fee95a1%3A0x8610e12d1d9dc0ac!2sLaboratorium%20Budownictwa%E2%80%A6!3m2!1d50.2905893!2d18.67881!4m5!1s0x47113101aa29eacb%3A0xb264c95270614eda!2zUG9saXRlY2huaWthIMWabMSFc2thLCBXeWR6aWHFgiBNYXRlbWF0eWtp4oCm!3m2!1d50.291104499999996!2d18.6800434!5e0!3m2!1sen!2spl!4v1725819994206!5m2!1sen!2spl'},
+]);
